e8dc88be9e6e8921a79bf412cbbb976e7d834376
izzat <nugmanov.i.j@gmail.com>
2021-09-02 14:46:54 +0500

    main.index: show user icon if currentUser exist

--- app/_pods/components/header/component.js
+++ app/_pods/components/header/component.js
@@ -2,8 +2,12 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object'
+import { alias } from '@ember/object/computed';
 
 export default class Comments extends Component {
+    @service('current-user') currentUserService;
+    @alias('currentUserService.user') currentUser;
+
     @tracked isMenuOpen = false;
 
     @action
