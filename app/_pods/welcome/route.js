3d96c8a79504a5bd66deb465bfb14bf3e6aebcb5
izzat <nugmanov.i.j@gmail.com>
2021-09-03 11:33:23 +0500

    welcome: hide user/company if user is authenticated

--- app/_pods/welcome/route.js
+++ app/_pods/welcome/route.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { alias } from '@ember/object/computed';
 
 export default class WelcomeRoute extends Route {
+  @service('current-user') currentUserService;
+  @alias('currentUserService.user') currentUser;
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    controller.currentUser = this.currentUser;
+  }
 }
