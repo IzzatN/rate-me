0896c6bf70ceec6ac8d2f446746a79682ecbe8ba
izzat <nugmanov.i.j@gmail.com>
2021-09-14 11:30:13 +0500

    transition to welcome route if authenticated user tries to go to register route

--- app/_pods/register/route.js
+++ app/_pods/register/route.js
@@ -1,13 +1,20 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 import Changeset from 'ember-changeset';
 import lookupValidator from 'ember-changeset-validations';
 import RegisterValidation from 'rate-me/validations/register';
 
 export default class RegisterRoute extends Route {
+  @service session;
+
   model() {
     return this.store.createRecord('user');
   };
+
+  beforeModel() {
+    this.session.prohibitAuthentication('welcome');
+  }
 
   setupController(controller, model) {
     super.setupController(...arguments);
