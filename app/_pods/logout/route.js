edf69ae5193a5badbecedb0b14c175bc2931909b
izzat <nugmanov.i.j@gmail.com>
2021-09-14 12:22:33 +0500

    update-welcome-route

--- app/_pods/logout/route.js
+++ app/_pods/logout/route.js
@@ -1,12 +1,15 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { later } from '@ember/runloop';
 
 export default class RegisterRoute extends Route {
   @service session;
 
   redirect() {
     return this.session.invalidate().then(() => {
-      return this.transitionTo('welcome');
+      later(() => {
+        return this.transitionTo('welcome');
+      }, 100);
     });
   }
 };
