9a9103580af3690b86fe9da7d06b492285a4c30c
izzat <nugmanov.i.j@gmail.com>
2021-09-14 14:22:36 +0500

    update-welcome-rout

--- app/_pods/business/add-service/controller.js
+++ app/_pods/business/add-service/controller.js
@@ -2,27 +2,24 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service'
 import { action } from '@ember/object'
-
+import { alias } from '@ember/object/computed';
 
 export default class AddServiceController extends Controller {
-    @service store;
-    @tracked name;
-    @tracked description;
-    @tracked categoryName;
-    @tracked services = [];
+  @service('current-user') currentUserService;
+  @alias('currentUserService.user') currentUser;
+  @service store;
+  @tracked name;
+  @tracked description;
+  @tracked categoryName;
+  @tracked services = [];
 
-    // get comments() {
-    //     return this.store.query('comment', { serviceId: this.serviceId });
-    // }
+  @action
+  async addService() {
+    let company = await this.store.findRecord('company', this.currentUser.company.id);
+    let service = this.store.createRecord('service', { name: this.name, description: this.description, company: company });
 
-    @action
-    async addService() {
-        let company = await this.store.findRecord('company', 1);
-        let service = this.store.createRecord('service', { name: this.name, description: this.description, company: company });
-
-        await service.save();
-        this.name = '';
-        this.description = '';
-    }
-
+    await service.save();
+    this.name = '';
+    this.description = '';
+  }
 }
--- app/tailwind/config.js
+++ app/tailwind/config.js
@@ -5,6 +5,17 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        oswald: ['Oswald', 'sans-serif'],
+        robotoSlab: ['Roboto Slab', 'sans-serif'],
+        abrilFatface: ['Abril Fatface', 'sans-serif'],
+        karantina: ['Karantina', 'sans-serif'],
+        abel: ['Abel', 'sans-serif'],
+        bebasNeue: ['Bebas Neue', 'sans-serif']
+      },
       extend: {
         colors: {
           sky: colors.sky,
