7592121c747e294436d3dbb312bc9edcc37a6390
izzat <nugmanov.i.j@gmail.com>
2021-09-18 01:05:05 +0500

    add ratings column to table/old

--- app/models/service.js
+++ app/models/service.js
@@ -7,6 +7,12 @@ export default class ServiceModel extends Model {
   @attr('date') updatedAt;
   @attr('string') rank;
 
+  @computed('rank')
+  get totalRank() {
+    let num = parseFloat(this.rank);
+    return Math.floor(num * 2) / 2;
+  }
+
   @belongsTo() company;
   @hasMany() categories;
 }
